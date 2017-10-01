516eccb768f5c51d7f8d7711a1d9733ea2bca8a2
Bruce <brucewmacdonald@gmail.com>
2017-10-01 12:58:41 -0300

    separate data generation from controller

--- dota2-predictions.py
+++ dota2-predictions.py
@@ -1,9 +1,8 @@
 """
 Data analysis tool to predict potential Dota 2 matches
+Run to generate results before using endpoint
 """
 
-from flask import Flask, jsonify
-app = Flask(__name__)
 import pandas as pd
 import math
 import csv
@@ -20,35 +19,6 @@ team_elos = {}
 ids_to_names = {}
 data_output = {}
 data_output_file = 'team-stats.json'
-
-
-#TODO: Clean up endpoints, make Flask structure
-@app.route('/odds/<team_a_id>/<team_b_id>', )
-def match_odds(team_a_id, team_b_id):
-    probability_a = get_probability_by_id(team_a_id, team_b_id)
-    probability_b = 1 - probability_a
-    moneyline_a = convert_to_moneyline_odds(probability_a)
-    moneyline_b = convert_to_moneyline_odds(probability_b)
-    name_a = convert_id_to_team_name(team_a_id)
-    name_b = convert_id_to_team_name(team_b_id)
-
-    # create a readable response
-    result = {}
-
-    result[team_a_id] = []
-    result[team_a_id].append({
-        'probability-to-win': str(probability_a),
-        'moneyline': str(moneyline_a),
-        'team-name' : name_a
-    })
-    result[team_b_id] = []
-    result[team_b_id].append({
-        'probability-to-win': str(probability_b),
-        'moneyline': str(moneyline_b),
-        'team-name' : name_b
-    })
-
-    return jsonify(result)
 
 
 def get_elo(team):
@@ -119,55 +89,6 @@ def analyze_team_stats():
         # w_performance = w_kills - w_deaths + w_assists
 
         update_elo(w_team_id, l_team_id)
-
-
-def get_probability_by_id(team_a, team_b):
-    # read values from json file serving as mock database
-    # team A and B passed by ID
-    with open(data_folder + data_output_file) as json_file:
-        data = json.load(json_file)
-        for p in data[str(team_a)]:
-            rating_a = int(p['elo'])
-        for p in data[str(team_b)]:
-            rating_b = int(p['elo'])
-
-    return probability_a_beats_b(rating_a, rating_b)
-
-
-def probability_a_beats_b(rating_a, rating_b):
-    # method from https://fivethirtyeight.com/features/introducing-nfl-elo-ratings/
-    # ELO probability converts to P(A) = 1/(1+10^(m))
-
-    m = (rating_b - rating_a) / 400
-
-    probability_a_wins = 1 / (1 + math.pow(10, m))
-
-    return probability_a_wins
-
-
-def convert_to_moneyline_odds(prob):
-    # convert probability to percent
-    percent_chance = prob * 100
-    # calculation depends on if probability is above 50% or below
-    if percent_chance >= 50:
-        odds = - ( percent_chance / (100 - percent_chance)) * 100
-    else:
-        odds = ((100 - percent_chance) / percent_chance) * 100
-
-    return odds
-
-
-def convert_id_to_team_name(team_id):
-    # read values from json file serving as mock database
-    # team A and B passed by ID
-    # will set name if match found
-    id_name = "none"
-    with open(data_folder + data_output_file) as json_file:
-        data = json.load(json_file)
-        for p in data[str(team_id)]:
-            id_name = p['name']
-
-    return id_name
 
 
 if __name__ == "__main__":
